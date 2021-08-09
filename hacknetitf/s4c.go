c14a6212e57be7534eec1d62b7433044e68bd2c2
suremoon <ProtossGenius@gmail.com>
2021-08-09 21:26:17 +0800

    "some code about deal message."

--- hacknetitf/s4c.go
+++ hacknetitf/s4c.go
@@ -1,16 +1,24 @@
 package hacknetitf
 
 import (
+	"fmt"
 	"net"
 
 	"github.com/ProtossGenius/hacknet/hnlog"
+	"github.com/ProtossGenius/hacknet/pb/hmsg"
+	"github.com/ProtossGenius/hacknet/pb/sc"
+	"github.com/ProtossGenius/hacknet/pb/smn_dict"
 	"github.com/ProtossGenius/hacknet/pinfo"
 	"github.com/sirupsen/logrus"
+	"google.golang.org/protobuf/internal/errors"
+	"google.golang.org/protobuf/proto"
 )
 
 const (
 	// ErrNoSuchHacker can't found this hacker by email.
 	ErrNoSuchHacker = "ErrNoSuchHacker"
+	// ErrUnexceptEnum enum not in except in this deal function.
+	ErrUnexceptEnum = "ErrUnexceptEnum"
 )
 
 // s4cImpl ServerForClientItf's impl.
@@ -37,17 +45,66 @@ func (s *s4cImpl) Hack(hackerEmail string, hackerAddr *net.UDPAddr, targetEmail 
 	return ""
 }
 
+// details log info's details.
+type details map[string]interface{}
+
+// MaxPackageSize udp package's max size.
+const MaxPackageSize = 1024
+
+func (s *s4cImpl) dealPackage(data []byte, remoteAddr *net.UDPAddr, err error) (string, details, error) {
+	if err != nil {
+		return "accept package.", details{}, err
+	}
+
+	wrapError := func(err error) error {
+		return fmt.Errorf("s4cImpl.dealPackage, Error : %w", err)
+	}
+
+	hnlog.Info("accept data", logrus.Fields{"remoteAddr": remoteAddr, "readSize": len(data), "message": data})
+
+	msg := new(hmsg.Message)
+	// TODO: decode to protoc and do something.
+	err = proto.Unmarshal(data, msg)
+	if err != nil {
+		return "unmarshal msg", details{}, wrapError(err)
+	}
+
+	switch msg.Enum {
+	case int32(smn_dict.EDict_sc_AnsHack):
+		ansHackMsg := new(sc.AnsHack)
+		err = proto.Unmarshal([]byte(msg.Msg), ansHackMsg)
+
+		if err != nil {
+			return "unmarshal msg.Msg", details{"msg.Enum": smn_dict.EDict_sc_AnsHack, "msg.Msg": msg.Msg}, wrapError(err)
+		}
+
+	default:
+		return "unknow Enum", details{"msg.Enum": msg.Enum, "msg.Msg": msg.Msg}, wrapError(errors.New(ErrUnexceptEnum))
+	}
+
+	return "", nil, nil
+}
+
 func (s *s4cImpl) startUp() {
-	bytes := make([]byte, 1024)
+	bytes := make([]byte, MaxPackageSize)
+
 	for {
 		n, remoteAddr, err := s.binder.ReadFromUDP(bytes)
-		if err != nil {
-			hnlog.Error(err.Error(), logrus.Fields{})
-			continue
-		}
-		hnlog.Info("accept data", logrus.Fields{"remoteAddr": remoteAddr, "readSize": n, "message": bytes[0:n]})
 
-		// TODO: decode to protoc and do something.
+		go func() {
+			data := make([]byte, n)
+			copy(data, bytes[0:n])
+
+			if info, fields, err := s.dealPackage(data, remoteAddr, err); err != nil {
+				if fields == nil {
+					fields = details{}
+					fields["fields_not_init"] = true
+				}
+
+				fields["error"] = err
+				hnlog.Error(info, fields)
+			}
+		}()
 	}
 }
 
